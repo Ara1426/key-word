76a50282842a38c6c548df9b61580f47c9cce0a5
Ara1426 <dilanlateef@gmail.com>
2024-11-12 19:10:19 +0300

    Add files via upload

---
+++ ca.py
@@ -0,0 +1,8 @@
+m=input ("enter a word")
+
+for i in m:
+    if i=='a':
+        print('a is found')
+        break
+    else:
+        print("a is not found")
---
+++ kp.py
@@ -0,0 +1,11 @@
+for d in range(10):
+    if d % 20 ==0:
+        print("twist")
+    elif d % 15 ==0:
+        pass
+    elif d % 5 ==0:
+        print("fizz")
+    elif d % 3 ==0:
+        print("buzz")
+    else:
+        print(d)
